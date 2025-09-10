d2e632a81fe06e6eae0740ee219066dbe736d40c
sonddlzb <sonddlzb@gmail.com>
2025-09-10 13:49:19 +0700

    init commit

---
+++ DemoCICD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoCICD
+//
+//  Created by Đào Đăng Sơn on 10/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoCICD/DemoCICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCICDApp.swift
+//  DemoCICD
+//
+//  Created by Đào Đăng Sơn on 10/9/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoCICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
